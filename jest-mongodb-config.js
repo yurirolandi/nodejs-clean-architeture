3dd0bd245ba71952e565e114ee9a73062b708b25
Rolandi <sd.rolandi@gmail.com>
2021-12-16 09:49:27 -0300

    chore: add jest configs for mongodb

---
+++ jest-mongodb-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    mongodbMemoryServerOptions: {
+      binary: {
+        version: '4.0.3',
+        skipMD5: true,
+      },
+      autoStart: false,
+      instance: {},
+    },
+  };
+
+  // version: '4.0.3', verificar a versao do mongodb em produção
+  
